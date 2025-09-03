fca4b0e078b9305f9cd73113eb2869a853385c3b
matheuspinto4 <mat.vsp@gmail.com>
2025-09-03 14:04:20 -0300

    Adicionei uns arquivos para ajudar

--- fieldProject/bndes/bcb.py
+++ fieldProject/bndes/bcb.py
@@ -4,6 +4,7 @@ import requests
 
 def get_normativo(numero_normativo):
     url = f"https://www.bcb.gov.br/api/conteudo/app/normativos/exibeoutrasnormas?p1=COMUNICADO&p2={numero_normativo}"
+    
     headers = {
     "Host": "www.bcb.gov.br",
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:142.0) Gecko/20100101 Firefox/142.0",
@@ -16,6 +17,7 @@ def get_normativo(numero_normativo):
     "Sec-Fetch-Mode": "cors",
     "Sec-Fetch-Site": "same-origin",
     }
+    
     page = requests.get(url, headers=headers)
     conteudo_lei = BeautifulSoup(page.json()['conteudo'][0]['Texto'], 'lxml')
     texto_lei = conteudo_lei.get_text(separator='\n', strip=True)
